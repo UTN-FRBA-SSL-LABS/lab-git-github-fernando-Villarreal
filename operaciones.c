d0dc3441d114acaee8c3bbeb722ee95e47b11af1
Villarreal Fernando <fvillarreal@frba.utn.edu.ar>
2026-05-01 20:56:53 -0300

    Revert "wip: experimento roto"
    This reverts commit c19c54124908a87cf7817a07541a391db9a0e0ba.

--- operaciones.c
+++ operaciones.c
@@ -23,7 +23,3 @@ int multiplicar(int a, int b) { //multiplicar dos números enteros
 int esPar(int n) {
     return (n % 2) == 0;
 }
-
-int dividir(int a, int b) {
-    return a - b; /* bug intencional */
-}
