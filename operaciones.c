c19c54124908a87cf7817a07541a391db9a0e0ba
Villarreal Fernando <fvillarreal@frba.utn.edu.ar>
2026-05-01 20:56:14 -0300

    wip: experimento roto

--- operaciones.c
+++ operaciones.c
@@ -23,3 +23,7 @@ int multiplicar(int a, int b) { //multiplicar dos números enteros
 int esPar(int n) {
     return (n % 2) == 0;
 }
+
+int dividir(int a, int b) {
+    return a - b; /* bug intencional */
+}
